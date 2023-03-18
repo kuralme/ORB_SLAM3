8ce8d9dd361d1e845338012dd9a31a3a31bfadd3
Ege Kural <krlmeigen@gmail.com>
2023-03-18 14:24:59 +0300

    deleted final question stuff

--- run_scripts/finalq1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ..
-echo "Launching final Q1 with Monocular cam"
-./Examples/Monocular/finalq1 ./Vocabulary/ORBvoc.txt ./Examples/Monocular/finalq1.yaml ./Datasets/Final_Exam_Q1 ./Datasets/Final_Exam_Q1/timeStamps/ftimes.txt dataset-finalq1
-
-
